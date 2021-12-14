6ca4ad2a77ca68f24986e506ca5c11cb4e1e1b6e
Rorical <hash_548@163.com>
2021-12-14 21:21:38 +0800

    feat: add fetchCancleAuction

--- src/auction-info.js
+++ src/auction-info.js
@@ -5,13 +5,23 @@ import {
   validateERC1155Json,
   getKnownProperties,
 } from './utils/utils'
-import AnyMoeAuctionCreateAuctionABI from './extra/AnyMoeAuction-CreateAuction-ABI.json'
+import AnyMoeAuctionABI from './extra/AnyMoeAuctionABI.json'
 
+const AnyMoeAuctionCancleAuctionABI = AnyMoeAuctionABI[1]
+const AnyMoeAuctionCancleAuctionTopic = Web3EthAbi.encodeEventSignature(
+  AnyMoeAuctionCancleAuctionABI,
+)
+
+const AnyMoeAuctionCreateAuctionABI = AnyMoeAuctionABI[2]
 const AnyMoeAuctionCreateAuctionTopic = Web3EthAbi.encodeEventSignature(
   AnyMoeAuctionCreateAuctionABI,
 )
 
 async function fetchNewAuctionInfo(mongodb, lbh, nbh) {
+  const Auction = mongodb
+    .mongoClient('mongodb-atlas')
+    .db('AnyMoe')
+    .collection('Auction')
   let logs = (
     await fetchEthereum({
       method: 'eth_getLogs',
@@ -34,7 +44,7 @@ async function fetchNewAuctionInfo(mongodb, lbh, nbh) {
       log['topics'],
     )
 
-    await Token.insertOne(
+    await Auction.insertOne(
         {
             "_id": CreateAuctionLog["auctionId"],
             "owner": CreateAuctionLog["owner"],
@@ -48,4 +58,42 @@ async function fetchNewAuctionInfo(mongodb, lbh, nbh) {
         }
     )
   }
+}
+
+async function fetchCancleAuction(mongodb, lbh, nbh) {
+  const Auction = mongodb
+    .mongoClient('mongodb-atlas')
+    .db('AnyMoe')
+    .collection('Auction')
+  let logs = (
+    await fetchEthereum({
+      method: 'eth_getLogs',
+      params: [
+        {
+          fromBlock: lbh,
+          toBlock: nbh,
+          topics: [AnyMoeAuctionCancleAuctionTopic],
+          address: ANYMOENFT_CONTRACT_ADDRESS,
+        },
+      ],
+    })
+  )['result']
+  for (let log of logs) {
+    if (log['data'] === '0x') continue
+    log['topics'].shift()
+    let CancleAuctionLog = Web3EthAbi.decodeLog(
+      AnyMoeAuctionCancleAuctionABI['inputs'],
+      log['data'],
+      log['topics'],
+    )
+
+    await Auction.deleteOne({
+      "_id": CancleAuctionLog["auctionId"]
+    })
+  }
+}
+
+export {
+  fetchNewAuctionInfo,
+  fetchCancleAuction
 }
